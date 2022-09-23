f8eb98d0f656333503cd4029ff2133a9902b2d98
Ryan Miller <ryanmillergm@gmail.com>
2022-09-23 14:52:00 -0600

    Adds show and store funciton in image controller and routes

--- app/Http/Controllers/ImageController.php
+++ app/Http/Controllers/ImageController.php
@@ -11,4 +11,14 @@ class ImageController extends Controller
     {
         return view('images.index');
     }
+
+    public function show()
+    {
+        // return all images
+    }
+
+    public function store(Request $request)
+    {
+        // save the image to the database
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -4,4 +4,6 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ImageController;
 
 Route::get('/', [ImageController::class, 'index']);
+Route::get('/images', [ImageController::class, 'show']);
+Route::post('/upload', [ImageController::class, 'store']);
 
