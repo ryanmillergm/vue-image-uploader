a7af62bc109b8b822d1cb521b7175f5dc9afdd88
Ryan Miller <ryanmillergm@gmail.com>
2022-09-24 09:03:49 -0600

    Instantiates FilePond and adds to components

--- resources/js/components/App.vue
+++ resources/js/components/App.vue
@@ -1,11 +1,25 @@
 <template>
     <div>
         <h2 class="text-xl font-bold text-center">{{ message }}</h2>
+        <file-pond
+            name="image"
+            ref="pond"
+            label-idle="Click to choose image, or drag here ..."
+            server="/upload"
+        />
     </div>
 
 </template>
 <script>
+    import vueFilePond from 'vue-filepond';
+    import "filepond/dist/filepond.min.css";
+
+    const FilePond = vueFilePond();
+
     export default {
+        components: {
+            FilePond
+        },
         data() {
             return {
                 message: 'Hello, world!'
