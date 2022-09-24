bb765537608a7c8a4c188c34639bef28a9f612cd
Ryan Miller <ryanmillergm@gmail.com>
2022-09-24 09:26:34 -0600

    Adds filepond-plugin-file-validate-type

--- resources/js/components/App.vue
+++ resources/js/components/App.vue
@@ -1,11 +1,12 @@
 <template>
-    <div>
-        <h2 class="text-xl font-bold text-center">{{ message }}</h2>
+    <div class="mt-4">
         <file-pond
             name="image"
             ref="pond"
             label-idle="Click to choose image, or drag here ..."
             server="/upload"
+            @init="filepondInitialized"
+            accepted-file-types="image/*"
         />
     </div>
 
@@ -13,8 +14,9 @@
 <script>
     import vueFilePond from 'vue-filepond';
     import "filepond/dist/filepond.min.css";
+    import FilePondPluginFileValidateType from 'filepond-plugin-file-validate-type';
 
-    const FilePond = vueFilePond();
+    const FilePond = vueFilePond(FilePondPluginFileValidateType);
 
     export default {
         components: {
@@ -22,7 +24,13 @@
         },
         data() {
             return {
-                message: 'Hello, world!'
+            }
+        },
+
+        methods: {
+            filepondInitialized() {
+                console.log('Filepond is ready!');
+                console.log('Filepond is object:', this.$refs.pond);
             }
         }
     }
